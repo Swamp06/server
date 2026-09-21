0e53fe6a4af547edd8a42f9edba27fbd6cbb5608
Евлоев Абдул-Рахим Т <student166@ithub.local>
2026-09-21 13:48:47 +0300

    Первый сервер обновление

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const http = require("http") // подключаем модуль http
+const express = require("express") // подключаем модуль express
 
+const app = express()
 const port = 3000 // порт на котором запускается сервер
 
-const server = http.createServer((req , res)=>{ // создаем сервер
-    res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер
-
+app.get("/" , (req , res)=>{
+    res.send("Главная страница")
+})
+app.get("/about" , (req , res)=>{
+    res.send("О нас")
 })
 
-server.listen(port, ()=>{ // тут расписываем на каком порту запускается сервер
+
+// const server = http.createServer((req , res)=>{ // создаем сервер
+//     res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер
+
+// })
+
+app.listen(port, ()=>{ // тут расписываем на каком порту запускается сервер
     console.log(`http://localhost:${port}`); // тут делаем уведомление для себя что сервер запущен
     
 })
