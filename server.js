51d4a6f5db271d92d2737c51c7043b6f657060bf
Евлоев Абдул-Рахим Т <student166@ithub.local>
2026-09-22 10:19:38 +0300

    POST и GET

--- server.js
+++ server.js
@@ -7,14 +7,39 @@ const port = 3000 // порт на котором запускается сер�
 app.get("/" , (req , res)=>{
     res.send("Главная страница")
 })
+
 app.get("/about" , (req , res)=>{
     res.send("О нас")
 })
 
+app.get("/contacts" , (req , res)=>{
+    res.send("Контакты")
+})
+
+app.get("/product/:id" , (req , res)=>{
+    console.log(req.params.id);
+    
+
+    res.send(`Продукт id: ${req.params.id}`)
+})
+
+app.get("/phone/:numbers" , (req , res)=>{
+    console.log(req.params.numbers);
+    
+
+    res.send(`Номер телефона: ${req.params.numbers}`)
+})
+
+app.post("/addproduct" , (req , res) =>{
+    const {name , age } = req.body
+
+    
+
+} )
+
 
 // const server = http.createServer((req , res)=>{ // создаем сервер
 //     res.end("Hello World") // ответ клиенту когда он зайдет на наш сервер
-
 // })
 
 app.listen(port, ()=>{ // тут расписываем на каком порту запускается сервер
